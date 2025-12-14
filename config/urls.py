f1d7c103011ad911f63894f76af480330198bd6e
athul <athul0752003@gmail.com>
2025-12-14 20:42:53 +0530

    login_page_set

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.home.urls")),  # landing page
+    path("users/",include("apps.users.urls")),
 
 ]
